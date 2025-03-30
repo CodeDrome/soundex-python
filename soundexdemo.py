6bea5083f8c1a1091b6abd7bee979c7964fb7be4
Chris Webb <50113303+CodeDrome@users.noreply.github.com>
2025-03-30 14:57:49 +0100

    Add files via upload

--- soundexdemo.py
+++ soundexdemo.py
@@ -16,14 +16,13 @@ def main():
 
     names2 = ["Jonson", "Addams", "Davies", "Simmons", "Richardson", "Tailor", "Chater", "Stephenson", "Naylor", "Smythe", "MacDonald", "Harrys", "Sym", "Wilson", "Barker", "Wills", "Frazer", "Johns", "Wilkinson", "Hunter", "Saunders", "Pearson", "Robertson", "Parker"]
 
-    namecount = len(names1)
+    # namecount = len(names1)
 
     for i in range(0, len(names1)):
 
         s1 = soundex.soundex(names1[i])
         s2 = soundex.soundex(names2[i])
 
-        # print("{:20s}{:4s}  {:20s}{:4s}".format(names1[i], s1, names2[i], s2))
         print(f"{names1[i]:20s}{s1:4s}  {names2[i]:20s}{s2:4s}")
 
 
